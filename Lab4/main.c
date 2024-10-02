82041dd9950abc340441d0a88858ac4842df2112
Noah Ross <me@noah.dev>
2024-10-02 14:34:44 -0400

    started lab4 code

---
+++ Lab4/main.c
@@ -0,0 +1,21 @@
+#include "pico/stdlib.h"
+#include "stdint.h"
+#include "stdbool.h"
+#include "stdlib.h"
+
+void main()
+{
+    int x = -1;
+    int y = -1;
+
+    while (1)
+    {
+
+        // read touch screen here
+
+        if (x > 0 && y > 0)
+        {
+            // main loop here
+        }
+    }
+}
---
+++ Lab4/ts_lcd.c
@@ -0,0 +1,16 @@
+#include "stdint.h"
+#include "TFTMaster.h"
+#include "TouchScreen.h"
+
+bool get_ts_lcd(uint16_t *px, uint16_t *py)
+{
+}
+
+void ts_lcd_init()
+{
+    tft_init_hw();
+}
+
+void ts_test()
+{
+}
