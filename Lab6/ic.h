cfdc3aae23569da1b50ec9a29a9717e5fc2b8396
Noah Ross <me@noah.dev>
2024-11-06 15:47:36 -0500

    update pins

--- Lab6/ic.h
+++ Lab6/ic.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * ic - simple interrupt-drive input capture for R
  *
  *
@@ -10,8 +10,7 @@
 #include "pico/stdio.h"
 
 // specify GPIO pin used for input capture
-// NOTE: GPIO13 is Pico Pin 17
-#define IC_PIN 13
+#define IC_PIN 6
 
 #define NO_CAPTURE_READY 0xffffffff
 
@@ -19,18 +18,17 @@
 // this is used as a timeout value
 #define MIN_RPM 50
 // number of microseconds when MIN_RPM period is reached
-#define MIN_RPM_TIMEOUT_US (60*1000000) / MIN_RPM
+#define MIN_RPM_TIMEOUT_US (60 * 1000000) / MIN_RPM
 
-    // Configure Interrupt on IC_PIN
-    void ic_init();   
-    
-    // return the period measured by IC1 in us
-    uint32_t ic_getperiod();
-    
+// Configure Interrupt on IC_PIN
+void ic_init();
 
-    // return the estimated RPM of the input capture
-    // if called more before another capture edge arrives, return 0
-    
-    uint32_t ic_ticks2rpm(uint32_t ticks);
-    
-    uint32_t ic_getrpm();
+// return the period measured by IC1 in us
+uint32_t ic_getperiod();
+
+// return the estimated RPM of the input capture
+// if called more before another capture edge arrives, return 0
+
+uint32_t ic_ticks2rpm(uint32_t ticks);
+
+uint32_t ic_getrpm();
